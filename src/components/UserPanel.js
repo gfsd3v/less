a915c19027228135272bc14cde1ea04dcb279b46
unknown <gfsd3v@gmail.com>
2019-04-14 10:33:14 -0300

    UserPanel SideBar done

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,6 +1,14 @@
 import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import '../assets/scss/components/_userpanel.scss'
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText,
+} from '@trendmicro/react-sidenav'
+import '@trendmicro/react-sidenav/dist/react-sidenav.css'
 
 const UserPanel = ({ ...props }) => {
   const [sideCollapse, setSideCollapse] = useState(``)
@@ -20,33 +28,62 @@ const UserPanel = ({ ...props }) => {
   return (
     <Fragment>
       <div className="user-container">
-        <aside className={`user-sidebar ${sideCollapse}`}>
-          <ul className="icons">
-            <li>
-              <a href="#" className="icon fa-twitter">
-                <span className="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon fa-github">
-                <span className="label">GitHub</span>
-              </a>
-            </li>
-          </ul>
-        </aside>
+        <SideNav
+          style={{ position: `inherit`, background: `transparent` }}
+          onSelect={selected => {
+            // Add your code here
+          }}
+        >
+          <SideNav.Toggle />
+          <SideNav.Nav style={{ marginTop: `1.5rem` }} defaultSelected="home">
+            <NavItem eventKey="home">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-home"
+                  style={{ color: `white`, fontSize: `1.75em` }}
+                />
+              </NavIcon>
+              <NavText>
+                <h4>home</h4>
+              </NavText>
+            </NavItem>
+            <NavItem eventKey="buy">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-shopping-cart"
+                  style={{ color: `white`, fontSize: `1.75em` }}
+                />
+              </NavIcon>
+              <NavText>
+                <h4>Comprar</h4>
+              </NavText>
+            </NavItem>
+            <NavItem eventKey="history">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-history"
+                  style={{ color: `white`, fontSize: `1.75em` }}
+                />
+              </NavIcon>
+              <NavText>
+                <h4>Historico</h4>
+              </NavText>
+            </NavItem>
+            <NavItem eventKey="Sair">
+              <NavIcon>
+                <i
+                  className="fa fa-fw fa-power-off"
+                  style={{ color: `white`, fontSize: `1.75em` }}
+                />
+              </NavIcon>
+              <NavText>
+                <h4>Sair</h4>
+              </NavText>
+            </NavItem>
+          </SideNav.Nav>
+        </SideNav>
         <section className={`user-content ${contentFull}`}>
           <h2 className="major">Compras</h2>
-          <button onClick={toggleTeste}>Toggle</button>
         </section>
       </div>
     </Fragment>
